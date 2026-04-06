cc62e0db538adc6d3c67544eef31da14f9e1c58d
alexisczl <kzal.alexis@gmail.com>
2026-04-06 11:26:13 +0200

    Implement KNN

--- src/methods/knn.py
+++ src/methods/knn.py
@@ -6,12 +6,13 @@ class KNN(object):
     kNN classifier object.
     """
 
-    def __init__(self, k=1, task_kind="classification"):
+    def __init__(self, k=1, task_kind="classification", distance_metric="euclidean"):
         """
         Call set_arguments function of this class.
         """
         self.k = k
         self.task_kind = task_kind
+        self.distance_metric = distance_metric
 
     def fit(self, training_data, training_labels):
         """
@@ -28,12 +29,10 @@ class KNN(object):
         Returns:
             pred_labels (np.array): labels of shape (N,)
         """
+        self.training_data = training_data
+        self.training_labels = training_labels
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        pred_labels = self.predict(training_data)
         return pred_labels
 
     def predict(self, test_data):
@@ -45,9 +44,27 @@ class KNN(object):
         Returns:
             test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        test_labels = []
+
+        for x in test_data: 
+            if self.distance_metric == "euclidean": 
+                distances = np.sqrt(np.sum((self.training_data - x) ** 2, axis=1))
+            elif self.distance_metric == "manhattan":
+                distances = np.sum(np.abs(self.training_data - x), axis=1)
+            else: 
+                raise ValueError("distance_metric must be 'euclidean' or 'manhattan'")
+
+            nearest_indices = np.argsort(distances)[:self.k]
+            nearest_labels = self.training_labels[nearest_indices]
+
+            if self.task_kind == "classification":
+                pred_label = np.bincount(nearest_labels.astype(int)).argmax()
+            elif self.task_kind == "regression":
+                pred_label = np.mean(nearest_labels)
+            else: 
+                raise ValueError("task_kind must be 'classification' or 'regression'")
+
+            test_labels.append(pred_label)
+
+        test_labels = np.array(test_labels)
         return test_labels
