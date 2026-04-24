f9a7f7338fc9ece0a4b76b6d3e348dc1b06be315
nehachak1 <neha.chakraborty@epfl.ch>
2026-04-24 12:52:29 +0200

    changed logistic regression first plot title

--- main.py
+++ main.py
@@ -249,6 +249,7 @@ def main(args):
             print(f"Valid F1 = {best[5]:.6f}")
             
             # Plot 1: vary lr for a fixed max_iters
+            best = max(results, key=lambda x: x[5])
             fixed_iter = best[0]
             lrs = []
             train_accs = []
